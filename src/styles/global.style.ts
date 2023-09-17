f1315aca26467aac846d774bac9a55c96a09fbf5
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-09-17 19:42:01 -0400

    ref: applying global styles in root element

--- src/styles/global.style.ts
+++ src/styles/global.style.ts
@@ -1,16 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap');
-
   * {
     box-sizing: border-box;
     padding: 0;
     margin: 0;
   }
 
-  body {
-    font-family: 'Inter', sans-serif;
+  #root {
+    font-family: 'Inter', serif;
     color: #2e2e2e;
     background: #fafafa;
     min-height: 100vh;
